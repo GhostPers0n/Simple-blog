fef922f32d561f22bb5de2373f245fe4d46666e7
fuad <fuadsanin2665@gmail.com>
2025-01-11 15:26:42 +0530

    Add create logic

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -11,6 +12,17 @@ app.get('/', (req, res) => {
 
 app.get('/create', (req, res) => {
     res.render('create.ejs');
+});
+
+const blogs = [];
+let id = 1;
+
+app.post('/', (req,res) => {
+    const blog = {id: id, ...req.body};
+    blogs.push(blog);
+    console.log(blogs);
+    id++
+    res.render('index.ejs');
 });
 
 app.listen(port, () => {
