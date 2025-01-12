502df0f00e0e7b3399e65ef02a43483ffcdf48c7
fuad <fuadsanin2665@gmail.com>
2025-01-12 20:43:33 +0530

    add update logic

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/create', (req, res) => {
     res.render('create.ejs');
 });
 
-const blogs = [];
+let blogs = [];
 let id = 1;
 
 app.post('/', (req,res) => {
@@ -30,6 +30,29 @@ app.get('/blog/:id', (req, res) => {
     res.render('blogDetails.ejs', {blog});
 })
 
+app.get('/delete/:id', (req, res) => {;
+    const blogId = req.params.id;
+    const blog = blogs.find(b => b.id == blogId);
+    blogs = blogs.filter(b => b.id !== blog.id);
+    res.redirect('/');
+});
+
+app.get('/update/:id', (req, res) => {
+    const blog = blogs.find(b => b.id == req.params.id);
+    res.render('update.ejs', {blog})
+});
+
+app.post('/update/:id', (req, res) => {
+    const { id } = req.params
+    const { title, content} = req.body
+
+    const blog = blogs.find( b => b.id == id);
+
+    blog.title = title
+    blog.content = content;
+    
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
